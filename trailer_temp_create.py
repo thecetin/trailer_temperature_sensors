2e4ba5a4508e80f7e85d77652180bc100d92e505
yusuf cetin <yusufnitec@hotmail.com>
2023-03-27 09:00:30 +0200

    Update trailer_temp_create.py

--- trailer_temp_create.py
+++ trailer_temp_create.py
@@ -1,16 +1,37 @@
+import msvcrt
+from threading import Thread, Lock
 import random
 import time
+import os
+from filelock import FileLock, Timeout
 
 while True:
-    with open("temperature.txt", 'w') as temp_data:
-        temp_data.truncate(0)
-        for i in range(1,23):
-            rand_temp = str(round(random.uniform(5.0,25.0),1))
-            wrt = (f'000{i} {rand_temp} C')
-            temp_data.write(wrt)
-            temp_data.write('\n')
-    
-    temp_data.close()
+
+    file_path = "temperature.txt"
+    lock_path = "temperature.txt.lock"
+
+    lock = FileLock(lock_path, timeout=1)
+    with lock:
+        with open(file_path, "a") as f:
+            #f.truncate(0)
+            for i in range(1,23):
+                rand_temp = str(round(random.uniform(5.0,25.0),1))
+                wrt = (f'000{i} {rand_temp} C')
+                f.write(wrt)
+                f.write('\n')
+
+    lock.acquire()
+    try:
+        with open(file_path, "a") as f:
+            f.truncate(0)
+            for i in range(1,23):
+                rand_temp = str(round(random.uniform(5.0,25.0),1))
+                wrt = (f'000{i} {rand_temp} C')
+                f.write(wrt)
+                f.write('\n')
+    finally:
+        lock.release()
+
     time.sleep(5)
 
         
